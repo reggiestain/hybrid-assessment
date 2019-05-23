dc196924b1c89bf59f4f66c53cbaa3bcb25e10b6
reggiestain <reggiestain@gmail.com>
2019-05-23 13:17:55 +0200

    cleared config

--- app/Http/Controllers/Front/CheckoutController.php
+++ app/Http/Controllers/Front/CheckoutController.php
@@ -86,9 +86,6 @@ class CheckoutController extends Controller {
      */
     public function itn(Request $request, PaymentProcessor $payfast)
     {
-        echo 'test';
-        exit();
-        /*
         // Retrieve the Order from persistance. Eloquent Example. 
         $order = Order::where('payment_id', $request->get('m_payment_id'))->firstOrFail(); // Eloquent Example 
     
@@ -111,8 +108,6 @@ class CheckoutController extends Controller {
                 echo "default";
                 break;
         }
-         * 
-         */
     }       
 
 
--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,8 @@ Route::get('/cart/add/{id}/{size?}', ['as' => 'cart.add', 'uses' => 'Front\CartC
 Route::get('/policy', ['as' => 'policy', 'uses' => 'Front\PagesController@policy']);
 Route::get('/back', ['as' => 'back', 'uses' => 'Front\PagesController@back']);
 Route::get('/checkout', ['as' => 'checkout', 'uses' => 'Front\CartController@checkout']);
-Route::post('checkout/confirmPayment', ['as' => 'checkout.payment', 'uses' => 'Front\CheckoutController@confirm']);
-Route::get('/checkout/paymentNotice', ['as' => 'checkout.notice', 'uses' => 'Front\CheckoutController@itn']);
+Route::post('/checkout/confirmPayment', ['as' => 'checkout.payment', 'uses' => 'Front\CheckoutController@confirm']);
+Route::post('/checkout/paymentNotice', ['as' => 'checkout.notice', 'uses' => 'Front\CheckoutController@itn']);
 
 Route::get('/login', ['as' => 'login', 'uses' => 'Front\PagesController@login']);
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
